f7d16c5b3a306c5c9137ec2a8929eb7404c01ee1
reneceron <rene.ceron@tec.mx>
2023-10-14 01:50:41 -0600

    se hizo un cambio en mi fecha a utc

--- components/MiFecha.tsx
+++ components/MiFecha.tsx
@@ -6,10 +6,15 @@ import siteMetadata from '@/data/siteMetadata'
 
 
 export default function MiFecha({ date}) {  
+    const nueva = new Date(date);
+    const utcDate = new Date(nueva.getTime() + (nueva.getTimezoneOffset() * 60 * 1000));
+
+    const dateString = utcDate.toString();
+    
   return (
     <>
-      <div className="text-gray-500 dark:text-gray-400">
-        <time dateTime={date}>{formatDate(date, siteMetadata.locale)}</time>
+      <div className="text-gray-500 dark:text-gray-400">           
+        <time dateTime={dateString}>{formatDate(dateString, siteMetadata.locale)}</time>
       </div>
     </>
   )
